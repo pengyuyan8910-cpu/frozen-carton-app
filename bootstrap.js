4065dac3c4f1484b208c279dff7d01640c1eb8d1
pengyuyan8910-cpu <pengyuyan8910@gmail.com>
2026-08-19 22:25:00 +0800

    更新陈列图方向标识缓存版本

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 (async function loadFrozenCartonData(){
-  const DATA_VERSION = "20260819_layout_recalc_v1";
+  const DATA_VERSION = "20260819_layout_recalc_v2";
   const REVIEW_MARKER = "frozen_carton_open_replan_review_v1";
   const note = document.getElementById("dataNote");
   const setNote = msg => { if (note) note.textContent = msg; };
